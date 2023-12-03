31381e29fb9a881fe91ad563a1d313613a64e2b1
Mateusz Szebestik <szebestik@gmail.com>
2023-12-03 23:44:55 +0100

    Added preload links, added alt to thumbnail

--- src/modules/shared/components/video-card/video-card.tsx
+++ src/modules/shared/components/video-card/video-card.tsx
@@ -14,7 +14,7 @@ export function VideoCard({ video }: VideoCardProps) {
   return (
     <Link to={`/watch/${video.id}`} className={styles.link}>
       <div className={styles.container}>
-        <img className={styles.container__thumb} src='https://picsum.photos/200/112' loading="lazy" />
+        <img className={styles.container__thumb} src='https://picsum.photos/200/112' loading="lazy" alt="thumbnail" />
         <div className={styles.container__info}>
 					<div className={styles.container__title}>
 						<p className={styles.text}>{video.title ?? "Testowy tytuł Testowy tytuł Testowy tytuł Testowy tytuł Testowy tytuł "}</p>
